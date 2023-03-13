81b77485394052b4141ff6eddad490949805cbd5
andreikostsen <pks@tut.by>
2023-03-13 13:51:46 +0300

    todolist-api created

---
+++ src/api/todolist-api.ts
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+const settings = {
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'ef64ab43-3fa1-4fd0-9e2b-61a258b28cf8'
+    }
+}
+
+
+export const todolistAPI = {
+
+    getTodolist() {
+        const promise = axios.get(
+            `https://social-network.samuraijs.com/api/1.1/todo-lists/`,
+            settings
+        )
+        return promise
+    },
+
+    createTodolist(title: string) {
+
+        const promise = axios.post(
+            `https://social-network.samuraijs.com/api/1.1/todo-lists`,
+            { title: title },
+            settings
+        )
+        return promise
+    },
+
+    deleteTodolist(todolistId: string) {
+        const promise = axios.delete(
+            `https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`,
+            {todolistId: todolistId},
+            settings
+        )
+        return promise
+    },
+
+    updateTodolist(todolistId: string, title: string) {
+        const promise = axios.put(
+            `https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`,
+            {title: title},
+            settings
+        )
+        return promise
+    },
+
+
+
+
+}
